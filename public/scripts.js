2ffb732b9d6628c4256986f580ffe0c63cd1524b
Sophie Jiang <sjiang13@email.unc.edu>
2023-05-09 18:54:22 -0400

    server

--- public/scripts.js
+++ public/scripts.js
@@ -4,59 +4,47 @@
 
 function shots() {
     let check = document.getElementById('opponent');
-    let rpsOption = document.getElementById('rps').checked;
+    let rps = document.getElementById('rps').checked;
 
     // Show or hide radio buttons
     if (check.checked) {
-        if (rpsOption){
+        if (rps) {
             $(".input-container.rps").show();
             $(".input-container.rpsls").hide();
-            $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors']").show(); 
+            $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors']").show();
             $("label[class='input-container' for='Lizard'], label[class='input-container' for='Spock']").hide();
         } else {
             $(".input-container.rps").show();
             $(".input-container.rpsls").show();
-            $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors']").show(); 
+            $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors']").show();
             $("label[class='input-container' for='Lizard'], label[class='input-container' for='Spock']").show();
         }
     } else {
         $(".input-container.rps").hide();
-            $(".input-container.rpsls").hide();
-            $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors']").hide(); 
-            $("label[class='input-container' for='Lizard'], label[class='input-container' for='Spock']").hide();
-        }
+        $(".input-container.rpsls").hide();
+        $("label[class='input-container' for='Rock'], label[class='input-container' for='Paper'], label[class='input-container' for='Scissors']").hide();
+        $("label[class='input-container' for='Lizard'], label[class='input-container' for='Spock']").hide();
     }
-    
-function startOver()
-    {
-        document.getElementById("game_options").className = "active";
-        document.getElementById("shot_options").className = "inactive";
-        document.getElementById("result").className = "inactive";
-        document.getElementById("play").className = "active";
-        
-        // Uncheck all buttons
-        document.getElementById("rps").checked = false;
-        document.getElementById("rpsls").checked = false;
-        document.getElementById("opponent").checked = false;
-        document.getElementById("rock").checked = false;
-        document.getElementById("paper").checked = false;
-        document.getElementById("scissors").checked = false;
-        document.getElementById("lizard").checked = false;
-        document.getElementById("spock").checked = false;
-    }
+}
+
+function startOver() {
+    document.getElementById('userinput').reset();
+    document.getElementById('results').innerHTML = '';
+    shots();
+}
 
 
-async function playGame(){
+async function playGame() {
     let game = $('input[type=radio][name=game]:checked').val();
     let shot = $('input[type=radio][name=shot]:checked').val();
     let opponent = document.getElementById('opponent');
-    if (opponent.checked){
+    if (opponent.checked) {
         let response = await fetch(window.location.href + 'app/' + game + '/play' + shot);
         let result = await response.json();
         console.log(result);
 
         let results = document.getElementById('results');
-        if (result.error){
+        if (result.error) {
             results.innerHTML = result.error;
         } else {
             let you = `You: ${result.player}`;
